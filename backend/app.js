e97d23d7da509ef6b8e1ae4804146ece51b99169
rayanrfd <dakhiarayan@gmail.com>
2025-04-30 15:42:18 +0200

    client side js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 const app = express()
 const todoRouter = require('./routes/todo.js')
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(express.json())
 app.use(morgan('dev'))
 app.use('/todos', todoRouter)
---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+const createTaskForm = document.getElementById('create-task-form');
+
+const createTask = async (event) => {
+    event.preventDefault()
+    
+}
+
